d49a8fae5157f4e540f78dfa0df65dfada46d2ec
bambademe <cheikhbambademe@gmail.com>
2022-04-23 05:56:33 -0700

    Api response model bis

--- src/main/java/com/learning/serverapp/model/Response.java
+++ src/main/java/com/learning/serverapp/model/Response.java
@@ -1,10 +1,16 @@
 package com.learning.serverapp.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
+@Data
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
     protected LocalDateTime timeStamp;
     protected int StatusCode;
