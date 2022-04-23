6c943bb497f5e11abe62199b0fce2000cf9f4fb3
bambademe <cheikhbambademe@gmail.com>
2022-04-23 04:23:11 -0700

    model and enumeration

--- src/main/java/com/learning/serverapp/model/Server.java
+++ src/main/java/com/learning/serverapp/model/Server.java
@@ -1,9 +1,24 @@
 package com.learning.serverapp.model;
 
 import com.learning.serverapp.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(unique = true)
+    @NotEmpty(message = "An ip address can not be ampty or null")
     private String ipAddress;
     private String name;
     private String memory;
