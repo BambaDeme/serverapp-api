0f66fdb8bbc14d03f3f4a83032a7a3d895a88851
bambademe <cheikhbambademe@gmail.com>
2022-04-23 04:31:04 -0700

    repository bis

--- src/main/java/com/learning/serverapp/repo/ServerRepo.java
+++ src/main/java/com/learning/serverapp/repo/ServerRepo.java
@@ -4,5 +4,5 @@ import com.learning.serverapp.model.Server;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServerRepo extends JpaRepository<Server,Long> {
-    String findByIpAddress(String ipAddress);
+    Server findByIpAddress(String ipAddress);
 }
